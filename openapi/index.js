0ff078b2eb9417d400d411ec21a8fb0468836afd
bhangun <bhangunh@gmail.com>
2021-08-21 22:04:05 +0700

    succes generate flutter apps from kutilang

--- openapi/index.js
+++ openapi/index.js
@@ -1,46 +1,335 @@
 const GenBase = require('../core/base');
 const writeFiles = require('./files').writeFiles;
+//const getOpenApi = require('../core/utils').getOpenApi;
+const SwaggerParser = require("@apidevtools/swagger-parser");
+
 
 module.exports = class extends GenBase {
-    
+
     constructor(args, opts) {
         super(args, opts);
+        this.appsName =opts.appsName
     }
 
     prompting() {
         const prompts = [
             {
+                type: 'input',
+                name: 'path_api',
+                message: 'Url/path to your api doc (json/yaml)',
+                store: true
+            },
+            {
+                type: 'input',
+                name: 'packageName',
+                validate: input => (/^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/.test(input) ? true : 'The package name you have provided is not a valid Java package name.'),
+                message: 'What is your package name?',
+                // default: this.jhipsterAppConfig.packageName,
+                store: true
+            },
+            {
                 type: 'list',
-                name: 'stateManageType',
-                message: 'Which State-Management style do you want to use?',
+                name: 'android',
+                message: 'Which Android native code do you want to use?',
                 choices: [
                     {
-                        value: 'mobx',
-                        name: 'MobX state-management'
+                        value: 'kotlin',
+                        name: 'Kotlin'
                     },
                     {
-                        value: 'bloc',
-                        name: 'BLoC state-management'
+                        value: 'java',
+                        name: 'Java'
                     },
                 ],
-                default: 'mobx'
-            }
+                default: 'kotlin'
+            },
+            {
+                type: 'list',
+                name: 'ios',
+                message: 'Which iOS native code do you want to use?',
+                choices: [
+                    {
+                        value: 'swift',
+                        name: 'Swift'
+                    },
+                    {
+                        value: 'objc',
+                        name: 'Objective-C'
+                    },
+                ],
+                default: 'swift'
+            },
+             {
+                 type: 'list',
+                 name: 'stateManageType',
+                 message: 'Which State-Management style do you want to use?',
+                 choices: [
+                     {
+                         value: 'mobx',
+                         name: 'MobX state-management'
+                     },
+                     {
+                         value: 'bloc',
+                         name: 'BLoC state-management'
+                     },
+                 ],
+                 default: 'mobx'
+             }
         ];
 
         const done = this.async();
         this.prompt(prompts).then((props) => {
-            this.props = props;
-            done();
+
+            const params = props.path_api
+
+            SwaggerParser.validate(params, (err, api) => {
+                if (err) {
+                    console.error(err);
+                }
+                else {
+                    this.props = { 
+                        appsName: this.appsName, 
+                        baseName: props.appsName,
+                        packageName: props.packageName,
+                        packageFolder: this.appsName, 
+                        stateManageType: props.stateManageType,
+                        android: props.android,
+                        ios: props.ios,
+                        entities: mappingEntities(api.components.schemas) }
+                    done();
+                }
+            })
         });
     }
 
-    get writing() {
-        return writeFiles();
+    validate() {
+        this.composeWith(require.resolve('generator-jhipster-kutilang/generators/mobx'),this.props);
     }
+
+
+    get writingEntity() {
+       // const writeEntity = require('generator-jhipster-kutilang/generators/entity-mobx/files');
+
+        console.log('---------Call Entity 2---------')
+       // return writeEntity;
+    }
+
+}
+/* 
+
+function getOpenApi(params, props) {
+    //const params = this.props.path_api
+    SwaggerParser.validate(params, (err, api) => {
+        if (err) {
+            console.error(err);
+        }
+        else {
+            console.log("API name: %s, Version: %s", api.info.title, api.info.version);
+
+            //console.log(api)
+            //console.log(api.paths['/pet/{petId}/uploadImage'])
+            //console.log(api.paths['/pet/{petId}/uploadImage'].post.parameters)
+            //console.log(api.paths['/pet/findByStatus'])
+            //console.log(Object.keys(api.components))
+            // console.log(mappingEntities(api.components.schemas))
+            //console.log(mappingEntities(api.components.schemas)[6].fields)
+            //console.log(api.components.schemas.Pet)
+            //console.log(api.components.securitySchemes)
+            //return callback
+            //  writingEntity()
+
+            props = mappingEntities(api.components.schemas)
+        }
+    });
+} */
+
+function mappingRelationship(obj) {
+    const relationship = []
+    return relationship
+}
+
+function mappingFields(obj, entities) {
+    // console.log(obj)
+    const fields = []
+    Object.entries(obj.properties).forEach(field => {
+        // console.log(field)
+        fields.push({
+            fieldType: field[1].type,
+            fieldName: field[0],
+            fieldIsEnum: (field[1].enum) ? true : false,
+            fieldValues: field[1].enum,
+            fieldDescription: field[1].description
+        })
+    })
+    return fields
+}
+
+function mappingEntities(obj) {
+    const entities = []
+    Object.entries(obj).forEach(entity => {
+        entities.push({
+            entityName: entity[0],
+            entityClass: entity[0],
+            entityFolderName: entity[0],
+            entityFileName: entity[0],
+            enableTranslation: false,
+            fields: mappingFields(entity[1], obj)
+        })
+    })
+    return entities
+
 }
 
 
 /*
+
+curl -X 'GET' \
+  'https://petstore3.swagger.io/api/v3/pet/findByStatus?status=available' \
+  -H 'accept: application/xml'
+
+  <ArrayList>
+
+  <item>
+    <id>9688</id>
+    <category>
+      <id>1</id>
+      <name>dog</name>
+    </category>
+    <name>Doggie</name>
+    <photoUrls>
+      <photoUrl>http://pet.photo.com/super_pet</photoUrl>
+    </photoUrls>
+    <tags>
+      <tag>
+        <id>-8697</id>
+        <name>defaultTag</name>
+      </tag>
+    </tags>
+    <status>available</status>
+  </item>
+
+  <item>
+    <id>-6314</id>
+    <category>
+      <id>1</id>
+      <name>Dogs</name>
+    </category>
+    <name>Chickens Flora Cain</name>
+    <photoUrls>
+      <photoUrl>string</photoUrl>
+    </photoUrls>
+    <tags>
+      <tag>
+        <id>250</id>
+        <name>defaultTag</name>
+      </tag>
+    </tags>
+    <status>available</status>
+  </item>
+  </ArrayList>
+
+
+{
+  openapi: '3.0.2',
+  info: {
+    title: 'Swagger Petstore - OpenAPI 3.0',
+    description: 'This is a sample Pet Store Server based on the OpenAPI 3.0 specification.  You can find out more about\n' +
+      "Swagger at [http://swagger.io](http://swagger.io). In the third iteration of the pet store, we've switched to the design first approach!\n" +
+      "You can now help us improve the API whether it's by making changes to the definition itself or to the code.\n" +
+      'That way, with time, we can improve the API in general, and expose some of the new features in OAS3.\n' +
+      '\n' +
+      'Some useful links:\n' +
+      '- [The Pet Store repository](https://github.com/swagger-api/swagger-petstore)\n' +
+      '- [The source API definition for the Pet Store](https://github.com/swagger-api/swagger-petstore/blob/master/src/main/resources/openapi.yaml)',
+    termsOfService: 'http://swagger.io/terms/',
+    contact: { email: 'apiteam@swagger.io' },
+    license: {
+      name: 'Apache 2.0',
+      url: 'http://www.apache.org/licenses/LICENSE-2.0.html'
+    },
+    version: '1.0.6'
+  },
+  externalDocs: {
+    description: 'Find out more about Swagger',
+    url: 'http://swagger.io'
+  },
+  servers: [ { url: '/api/v3' } ],
+  tags: [
+    {
+      name: 'pet',
+      description: 'Everything about your Pets',
+      externalDocs: [Object]
+    },
+    { name: 'store', description: 'Operations about user' },
+    {
+      name: 'user',
+      description: 'Access to Petstore orders',
+      externalDocs: [Object]
+    }
+  ],
+  paths: {
+    '/pet': { put: [Object], post: [Object] },
+    '/pet/findByStatus': { get: [Object] },
+    '/pet/findByTags': { get: [Object] },
+    '/pet/{petId}': { get: [Object], post: [Object], delete: [Object] },
+    '/pet/{petId}/uploadImage': { post: [Object] },
+    '/store/inventory': { get: [Object] },
+    '/store/order': { post: [Object] },
+    '/store/order/{orderId}': { get: [Object], delete: [Object] },
+    '/user': { post: [Object] },
+    '/user/createWithList': { post: [Object] },
+    '/user/login': { get: [Object] },
+    '/user/logout': { get: [Object] },
+    '/user/{username}': { get: [Object], put: [Object], delete: [Object] }
+  },
+  components: {
+    schemas: {
+      Order: [Object],
+      Customer: [Object],
+      Address: [Object],
+      Category: [Object],
+      User: [Object],
+      Tag: [Object],
+      Pet: [Object],
+      ApiResponse: [Object]
+    },
+    requestBodies: { Pet: [Object], UserArray: [Object] },
+    securitySchemes: { petstore_auth: [Object], api_key: [Object] }
+  }
+}
+{
+  post: {
+    tags: [ 'pet' ],
+    summary: 'uploads an image',
+    description: '',
+    operationId: 'uploadFile',
+    parameters: [
+        {
+            name: 'petId',
+            in: 'path',
+            description: 'ID of pet to update',
+            required: true,
+            schema: { type: 'integer', format: 'int64' }
+        },
+        {
+            name: 'additionalMetadata',
+            in: 'query',
+            description: 'Additional Metadata',
+            required: false,
+            schema: { type: 'string' }
+        }
+    ],
+    requestBody: { content: [Object] },
+    responses: { '200': [Object] },
+    security: [ [Object] ]
+  }
+}
+
+*/
+
+
+/*
+
 {
   sharedData: {},
   forwardErrorToEnvironment: false,
