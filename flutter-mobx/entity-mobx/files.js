365ee6df4813063df9e9ce22e1e3efe0149524cd
bhangun <bhangunh@gmail.com>
2021-09-13 21:50:00 +0700

    generate screen got bug

--- flutter-mobx/entity-mobx/files.js
+++ flutter-mobx/entity-mobx/files.js
@@ -4,21 +4,22 @@ module.exports = {
 };
 
 
-function writeFiles(folder, obj, props) {
+function writeFiles(folder, obj, props, _) {
 
-    const entityFolderName = folder + '/lib/modules/' + option.entityClass
+    const entityFolderName = folder + '/lib/modules/' + props.entityClass
 
-    const entityFileName = entities.entityClass
-
-    obj.template('entity.ejs', `${folder}/lib/models/${entityFileName}.dart`, obj, props)
+    const entityFileName = props.entityClass
+    props.entity._ = _ 
+//console.log(props.entity)
+    obj.template('entity.ejs', `${folder}/lib/models/${entityFileName}.dart`, obj, props.entity)
 
     if (props.isPrintComponent) {
-        obj.template('entity.list.ejs', `${entityFolderName}/screens/${entityFileName}_list.dart`, obj, option)
-        obj.template('entity.detail.ejs', `${entityFolderName}/screens/${entityFileName}_detail.dart`, obj, option)
-        obj.template('entity.form.ejs', `${entityFolderName}/screens/${entityFileName}_form.dart`, obj, option)
-        obj.template('entity.services.ejs', `${entityFolderName}/services/${entityFileName}_services.dart`, obj, option)
-        obj.template('entity.store.ejs', `${entityFolderName}/store/${entityFileName}_store.dart`, obj, option)
-        obj.template('entity.route.ejs', `${entityFolderName}/services/${entityFileName}_routes.dart`, obj, option)
-        obj.template('entity.module.ejs', `${entityFolderName}/${entityFileName}_module.dart`, obj, option)
+        obj.template('entity.list.ejs', `${entityFolderName}/screens/${entityFileName}_list.dart`, obj, props)
+        obj.template('entity.detail.ejs', `${entityFolderName}/screens/${entityFileName}_detail.dart`, obj, props)
+        obj.template('entity.form.ejs', `${entityFolderName}/screens/${entityFileName}_form.dart`, obj, props)
+        obj.template('entity.services.ejs', `${entityFolderName}/services/${entityFileName}_services.dart`, obj, props)
+        obj.template('entity.store.ejs', `${entityFolderName}/store/${entityFileName}_store.dart`, obj, props)
+        obj.template('entity.route.ejs', `${entityFolderName}/services/${entityFileName}_routes.dart`, obj, props)
+        obj.template('entity.module.ejs', `${entityFolderName}/${entityFileName}_module.dart`, obj, props)
     }
 }
--- flutter-mobx/entity-mobx/index.js
+++ flutter-mobx/entity-mobx/index.js
@@ -11,17 +11,16 @@ module.exports = class extends GenBase {
     }
 
     writingEntity() {
-        this.props._ = this._
 
         if (this.props.entities.length>0) this.props.entities.forEach(e => {
-            this.props.entities = e
-            writeEntity(this.props.packageFolder, this, this.props)
+            this.props.entity = e   
+            writeEntity(this.props.packageFolder, this, this.props,  this._)
         })
-        else this.otherEntity(this.props.paths).forEach(e =>{
-            this.props.entities = e
-            writeEntity(this.props.packageFolder, this, this.props)
+        else this.otherEntity(this.props.paths).forEach(el =>{
+            this.props.entity = el
+            writeEntity(this.props.packageFolder, this, this.props,  this._)
         })
-        
+        this.props._ = this._
         this.props.paths.forEach((path,i) =>{
             writeOperation(this.props.packageFolder, path, this, i, this.props)
         })
